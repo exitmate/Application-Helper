c490496687ece866ff336825b3ad8791a9107a04
Gahyeon <best11gh@naver.com>
2025-08-29 17:37:33 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/exitmate/application_helper/ApplicationHelperApplication.java
@@ -0,0 +1,13 @@
+package com.exitmate.application_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exitmate/application_helper/ApplicationHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exitmate.application_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
